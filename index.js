6cfa8f8d30175d74e5d78b10807eebe2f24080e1
Kris de Rycke <derycke.kris@gmail.com>
2019-06-25 11:15:49 +0200

    randomize classnames

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 window.addEventListener("load", randomize);
 
 //adding click event on all divs with same classname
+
 var cards = document.getElementsByClassName("cardHidden");
 var cardsArray = Array.from(cards);
+var card = event.path[0];
 var i = 0;
 
 for (i; i < cardsArray.length; i++) {
@@ -19,4 +21,10 @@ function turn(event) {
 function randomize() {
   console.log("pics will be randomized");
   var pics = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6", "cat7"];
+  console.log(pics);
+  //randomize pics
+  randomPics = pics[Math.floor(Math.random() * pics.length)];
+  console.log(randomPics);
+  //add random classname to clicked div
+  card.className = randomPics;
 }
