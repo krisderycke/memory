ee4ad4610c37d86ece058f87d4e2afd41047cec2
Kris de Rycke <derycke.kris@gmail.com>
2019-06-25 11:02:02 +0200

    add classes for pictures

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ for (i; i < cardsArray.length; i++) {
 }
 
 function turn(event) {
-  event.path[0].className = "cardDeck";
+  var card = event.path[0];
+  card.className = "cardDeck";
 }
 
 function randomize() {
-  alert("pics will be randomized");
+  console.log("pics will be randomized");
+  picArray= [./assets/pics/cat1.jpg, ./assets/pics/cat2.jpg,./assets/pics/cat3.png,./assets/pics/cat4.jpg,./assets/pics/cat5.jpg,./assets/pics/cat6.jpg,./assets/pics/cat7.jpg]
 }
