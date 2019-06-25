21c309c5146d6b3bdf5ee9ac8086abe6785b8f19
Kris de Rycke <derycke.kris@gmail.com>
2019-06-25 16:22:23 +0200

    womment out all the excess code

--- index.js
+++ index.js
@@ -5,26 +5,28 @@ window.addEventListener("load", randomize);
 let cards = document.getElementsByClassName("cardHidden");
 let cardsArray = Array.from(cards);
 let pics = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6", "cat7"];
+let picsDouble = pics.concat(pics);
 
-for (let i = 0; i < cardsArray.length; i++) {
-  cardsArray[i].addEventListener("click", function() {
-    turn(event);
-    console.log("click");
-  });
+// for (let i = 0; i < cardsArray.length; i++) {//
+//   cardsArray[i].addEventListener("click", function() {
+//     turn(event);
+//     console.log("click");
+//   });//
 
-  function turn(event) {
-    var card = event.path[0];
-    // card.className = randomPics;
-  }
-}
+// function turn(event) {
+//   var card = event.path[0];
+//   card.className = randomPics;
+// }
+//}
 
 //start randomize function
 function randomize() {
-  let picsDouble = pics.concat(pics);
-  console.log(picsDouble);
-
-  // console.log("pics will be randomized");
   for (let i = 0; i <= picsDouble.length; i++) {
-    cardsArray[i].className = Math.floor(Math.random() * picsDouble[i]);
+    var picsDoubleRandom = Math.floor(Math.random(i) * cardsArray.length);
+    console.log(picsDoubleRandom);
+    console.log(cardsArray[i].classList.remove("cardHidden"));
+    console.log(cardsArray[i].classList.add(picsDoubleRandom));
+
+    // console.log(picsDouble[i]);
   }
 }
