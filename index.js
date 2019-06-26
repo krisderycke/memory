c221e5ad79b37002c0d60e9bb9c1b1a3128793ed
Kris de Rycke <derycke.kris@gmail.com>
2019-06-26 09:40:08 +0200

    nothing

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+// i have to select every div in my html file.(document.getElemantsByClassName)
+// put all the element in an Array (push)
+// (for each) value of my array i have to compare 2 of them.
+
+// (if) those value are the same then => "verify the value of the classname"
+// every pair of divs should have a classname (.catN)
+
 window.addEventListener("load", randomize);
 
 //adding click event on all divs with same classname
 
 let cards = document.getElementsByClassName("cardHidden");
 let cardsArray = Array.from(cards);
+
 let pics = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6", "cat7"];
-let picsDouble = pics.concat(pics);
-console.log(picsDouble); //equal quantity of elements in array of divs and araay of pics
+// let picsDouble = pics.concat(pics);
+// console.log(picsDouble); //equal quantity of elements in array of divs and araay of pics
 
 // for (let i = 0; i < cardsArray.length; i++) {//
 //   cardsArray[i].addEventListener("click", function() {
@@ -22,11 +30,9 @@ console.log(picsDouble); //equal quantity of elements in array of divs and araay
 
 //start randomize function
 function randomize() {
-  for (let i = 0; i <= picsDouble.length; i++) {
-    var picsDoubleRandom = Math.floor(Math.random(i) * cardsArray.length);
-    console.log(picsDoubleRandom);
-
-    
+  for (let i = 0; i <= cardsArray.length; i++) {
+    var picsRandom = [Math.floor(Math.random() * cardsArray.length)];
+    console.log(picsRandom);
 
     // console.log(cardsArray[i].classList.remove("cardHidden"));
     // console.log(cardsArray[i].classList.add(picsDoubleRandom));
