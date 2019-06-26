f40c8f119197532cdaf8e47a21b569fca1a95117
Kris de Rycke <derycke.kris@gmail.com>
2019-06-26 09:05:25 +0200

    console.log(equal quantity of elements in array of divs and araay of pics)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let cards = document.getElementsByClassName("cardHidden");
 let cardsArray = Array.from(cards);
 let pics = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6", "cat7"];
 let picsDouble = pics.concat(pics);
+console.log(picsDouble); //equal quantity of elements in array of divs and araay of pics
 
 // for (let i = 0; i < cardsArray.length; i++) {//
 //   cardsArray[i].addEventListener("click", function() {
@@ -24,9 +25,10 @@ function randomize() {
   for (let i = 0; i <= picsDouble.length; i++) {
     var picsDoubleRandom = Math.floor(Math.random(i) * cardsArray.length);
     console.log(picsDoubleRandom);
-    console.log(cardsArray[i].classList.remove("cardHidden"));
-    console.log(cardsArray[i].classList.add(picsDoubleRandom));
 
-    // console.log(picsDouble[i]);
+    
+
+    // console.log(cardsArray[i].classList.remove("cardHidden"));
+    // console.log(cardsArray[i].classList.add(picsDoubleRandom));
   }
 }
